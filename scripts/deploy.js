f742d613cbf1068b77d0a24a62797dd1c3754ad8
concave-dash <kwerkee12@gmail.com>
2021-12-03 11:23:37 -0500

    adding contract address on ropsten

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,8 @@
 async function main() {
-  const MyNFT = await ethers.getContractFactory("TheSpirals")
-  // contract address 0x6F69141C0419B1D94C29bD5972F99213C2CE7b92
+  const MyNFT = await ethers.getContractFactory("TheColors")
+  // Ropsten
+  // TheColors contract address 0x1cAA1167Ff1BD298d71A2E9b19aA5595C1fbC341
+  // TheSpirals contract address 0x6F69141C0419B1D94C29bD5972F99213C2CE7b92
 
   // Start deployment, returning a promise that resolves to a contract object
   const myNFT = await MyNFT.deploy()
---
+++ scripts/mint-nft.js
@@ -0,0 +1,9 @@
+require("dotenv").config()
+
+const API_URL = process.env.API_URL
+
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
+
+const web3 = createAlchemyWeb3(API_URL)
+
+const contract = require("../artifacts/contracts/legacy_spirals/TheSpirals.sol")
