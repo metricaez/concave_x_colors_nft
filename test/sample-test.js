39bd2471c96b74fd10b5f1438d2112fe908e1ada
concave-dash <kwerkee12@gmail.com>
2021-12-03 12:46:18 -0500

    add test

--- test/sample-test.js
+++ test/sample-test.js
@@ -3,7 +3,18 @@ const { ethers } = require("hardhat");
 
 
 // SAMPLE TEST of default hardhat project with "Greeter" contract, change to relevant use when needed. 
-describe("Greeter", function () {
+describe("Test", function () {
+  it("Should check mint", async function () {
+    const color_contact = await ethers.getContractFactory("TheColors");
+    const color = await color_contact.deploy();
+    console.log('delpoyed?');
+    const depl = await color.deployed();
+    console.log('delpoyed');
+    const mintTx = await color.mintNextColors(1);
+    await mintTx.wait();
+    console.log(mintTx);
+  });
+  /*
   it("Should return the new greeting once it's changed", async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello, world!");
@@ -18,4 +29,5 @@ describe("Greeter", function () {
 
     expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
+ */
 });
