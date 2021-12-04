a05e33487fdd00eb53eb4a785d2b082f0dcb794f
concave-dash <kwerkee12@gmail.com>
2021-12-04 12:42:22 -0500

    restoring optimization

--- hardhat.config.js
+++ hardhat.config.js
@@ -38,13 +38,11 @@ module.exports = {
    },
   solidity: {
     version: "0.8.4",
-    /*
     settings: {
       optimizer: {
         enabled: true,
         runs: 1000,
       },
     },
-   */
   },
 };
