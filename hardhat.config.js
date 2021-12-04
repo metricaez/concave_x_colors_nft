33b41031586eef91e07318778a914f1d58785751
concave-dash <kwerkee12@gmail.com>
2021-12-04 08:29:42 -0500

    disabling contract optimization for testing

--- hardhat.config.js
+++ hardhat.config.js
@@ -38,11 +38,13 @@ module.exports = {
    },
   solidity: {
     version: "0.8.4",
+    /*
     settings: {
       optimizer: {
         enabled: true,
         runs: 1000,
       },
     },
+   */
   },
 };
